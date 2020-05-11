90022f68128cb205019b194ec625875407952291
d7p4n4 <bercikezs199999999@gmail.com>
2020-05-11 16:19:06 +0200

    Add project files.

---
+++ CSEFTPTStrategyFW/Context.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSEFTPTStrategyFW
+{
+    public class Context : DbContext
+    {
+        public Context() : base("employeeConnectionString") {
+
+            Database.SetInitializer<Context>(new CreateDatabaseIfNotExists<Context>());
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ContractEmployee>().ToTable("ContractEmployees");
+            modelBuilder.Entity<PermanentEmployee>().ToTable("PermanentEmployees");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ CSEFTPTStrategyFW/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSEFTPTStrategyFW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ctx = new Context();
+
+            try
+            {
+                ctx.Employees.Add(new PermanentEmployee()
+                {
+                    FirstName = "Darth",
+                    LastName = "Vader",
+                    Gender = "male",
+                    AnnualSalary = 156230
+                });
+
+                ctx.SaveChanges();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
